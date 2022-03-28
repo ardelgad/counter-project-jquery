57f8ac5e7d536cf63d5cab041dc179747abb06c7
ardelgad <adelgadoporras@gmail.com>
2022-03-28 09:27:59 +0200

    done

--- js/app.js
+++ js/app.js
@@ -4,3 +4,33 @@ $(document).ready(function () {
     console.log("DOM cargado! Implementa tu solución a partir de esta línea.");
 
 });
+
+$(".prevBtn").click(function lowerNum() {
+    counter-=1
+    changeCounter();
+    changeColor();
+})
+
+$(".nextBtn").click(function riseNum() {
+    counter+=1
+    changeCounter();
+    changeColor();
+})
+
+
+function changeCounter() {
+    $("#counter").text(counter);    
+}
+
+function changeColor() {
+    if (counter>0) {
+        $("#counter").css('color', 'green');
+    }
+    else if (counter == 0) {
+        $("#counter").css('color', 'black');
+    }
+    
+    else {
+        $("#counter").css('color', 'red');
+    }
+}
